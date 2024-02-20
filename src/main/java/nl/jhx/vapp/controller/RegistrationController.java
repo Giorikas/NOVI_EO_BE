bfde4595e79b5c1653b4a30509eb742a2dfd0b3f
Giorikas <jjphendrikx@hotmail.com>
2024-02-20 20:59:27 +0100

    Postmapping toDo change String for Object

--- src/main/java/nl/jhx/vapp/controller/RegistrationController.java
+++ src/main/java/nl/jhx/vapp/controller/RegistrationController.java
@@ -8,11 +8,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/register")
 public class RegistrationController {
 
+    // De "String" hier hoort een object te zijn, dit object zit ergens in het programma gedefineerd op zo'n manier dat hij van JSON naar data van dit object kan gaan. De vraag is hoe definieer je dit object.
+    // Als dat lukt kun je even een klein simpel object maken met maar 1 member en kijken of de register of welke POST functie dan ook werkt. En als dat werkt kun je het uitbreiden naar wat je nodig hebt.
     @PostMapping()
-    public ResponseEntity<> register(@RequestBody String content)
+    public ResponseEntity<String> register(@RequestBody String content)
     {
-        return ResponseEntity.ok("VAPP POST is responding" + content);
+        return ResponseEntity.ok("wer");
     }
+
     // This works
     /*@PostMapping
     public ResponseEntity<String> postRegisterUser(String content){
