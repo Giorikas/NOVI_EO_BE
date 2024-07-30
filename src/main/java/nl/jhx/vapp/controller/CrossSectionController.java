d839be6f3503608de5e91f46c7bdf3d341d1c5c4
Giorikas <jjphendrikx@hotmail.com>
2024-07-30 23:13:48 +0200

    crosSection heel basic naar backend

--- src/main/java/nl/jhx/vapp/controller/CrossSectionController.java
+++ src/main/java/nl/jhx/vapp/controller/CrossSectionController.java
@@ -1,5 +1,6 @@
 package nl.jhx.vapp.controller;
 import nl.jhx.vapp.model.CrossSection;
+import nl.jhx.vapp.model.CrossSectionPart;
 import nl.jhx.vapp.repository.CrossSectionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -27,23 +28,35 @@ public class CrossSectionController {
     }
 
     @PostMapping
-    public ResponseEntity<CrossSection> createCrossSection(@RequestBody CrossSection crossSection){
-        crossSectionRepository.save(crossSection);
-        URI uri = URI.create(
-                ServletUriComponentsBuilder
-                        .fromCurrentRequest()
-                        .path("/" + crossSection.getId()).toUriString()
-        );
-        return ResponseEntity.created(uri).body(crossSection);
+    public void saveCrossSection(@RequestBody CrossSectionPart[] crossSectionParts){
+        System.out.println(crossSectionParts.length);
+
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<CrossSection> updateCrossSection(@PathVariable int id, @RequestBody CrossSection crossSection){
-        if (id >=0 && id < this.crossSections.size()) {
-            this.crossSections.set(id, crossSection);
-            return new ResponseEntity<>(crossSection, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-        }
-    }
+/*    @PostMapping
+    public void saveCrossSection(@RequestBody CrossSection crossSection){
+        crossSectionRepository.save(crossSection);
+
+    }*/
+
+//    @PostMapping
+//    public ResponseEntity<CrossSection> createCrossSection(@RequestBody CrossSection crossSection){
+//        crossSectionRepository.save(crossSection);
+//        URI uri = URI.create(
+//                ServletUriComponentsBuilder
+//                        .fromCurrentRequest()
+//                        .path("/" + crossSection.getId()).toUriString()
+//        );
+//        return ResponseEntity.created(uri).body(crossSection);
+//    }
+//
+//    @PutMapping("/{id}")
+//    public ResponseEntity<CrossSection> updateCrossSection(@PathVariable int id, @RequestBody CrossSection crossSection){
+//        if (id >=0 && id < this.crossSections.size()) {
+//            this.crossSections.set(id, crossSection);
+//            return new ResponseEntity<>(crossSection, HttpStatus.OK);
+//        } else {
+//            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+//        }
+//    }
 }
---
+++ src/main/java/nl/jhx/vapp/model/CrossSectionPart.java
@@ -0,0 +1,19 @@
+package nl.jhx.vapp.model;
+
+import jakarta.persistence.Column;
+
+public class CrossSectionPart {
+
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
